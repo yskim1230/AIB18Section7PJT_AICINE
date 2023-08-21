ac83150da24c7463934e5d1f4527c5c9980de4ac
yskim1230 <124799967+yskim1230@users.noreply.github.com>
2023-08-21 15:08:20 +0900

    Delete 기본모델 directory

--- 기본모델/ds-sa-recommendation-main/models/impersonal.py
+++
@@ -1,6 +0,0 @@
-from utils.Dataloader import load_ratings
-
-ratings_df = load_ratings('datasets')
-
-def popular(top=5):
-    return ratings_df.groupby('movieId').count()['userId'].sort_values(ascending=False).index.tolist()[:top]
